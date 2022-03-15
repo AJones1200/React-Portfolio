a54ae31a50c7c3b44f0e3e2aacd2a6900c0d735e
Ayse Jones <aysej305@gmail.com>
2022-03-14 22:01:34 -0500

    finished

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -4,7 +4,7 @@ export default function ProjectCard(props) {
   return (
     <div className= "cards">
       <h1>{props.project.title}</h1>
-      <img src= {props.project.image}/>
+      <img src= {props.project.image} alt={props.project.title}/>
       <p>{props.project.description}</p>
       <div className='links'>
       <a href = {props.project.deployedLink}>deployed link</a>
