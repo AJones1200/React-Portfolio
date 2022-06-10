dbceaf47e3d0ce32d2c6451760c8e4661d4dfe40
Ayse Jones <aysej305@gmail.com>
2022-06-09 21:27:32 -0500

    resume link added

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,7 +6,7 @@ export default function Footer(props) {
     <div class = "footer">
         <a href= "https://github.com/AJones1200">Github</a>
         <a href= "https://www.linkedin.com/in/ayse-jones-full-stack-developer/">LinkedIn</a>
-        <a href= "https://www.openpowerlifting.org/u/aysejones">OpenPowerlifting</a>
+        <a href= "https://sway.office.com/q8jlkHS6tPmBzcDO?ref=Link">Resume</a>
     </div>
   );
 }
